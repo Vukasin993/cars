1ff8ac9f19f65e3347e1eb82e09c3d20244124c2
Vukasin Macura <moj_email@gmail.com>
2020-08-28 21:09:14 +0200

    layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
-    <title>About</title>
+    <title>@yield('title')</title>
 </head>
 <body>
-    <h1 class="badge-success">About us</h1>
-   <script src="{{ URL::asset('js/app.js') }}"></script>
+    <div class=container>
+        @yield('content');
+    </div>
+    <script src="{{ URL::asset('js/app.js') }}"></script>
 </body>
 </html>
